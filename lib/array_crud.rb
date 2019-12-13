3e4a2fa408bc2111b633933221cf7dcde55c75e1
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-13 23:47:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array=["hello","good","well"])
-array = ["hello","good","well"]
+array = ["hello","arrays!","well"]
 array.pop
 end
 
