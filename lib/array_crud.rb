2549e2e953b39c0ab1203448a88c81b68c7d28a0
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-13 23:34:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,34 +4,37 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["hi","hello","how are you","good"]
+array =  ["hi","hello","how are you","good"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  ["hello","how are you","good","well"]
-  add_elemnt_to_end_of_array<<"goody"
+array=["hello","how are you","good","well"]
+element = "goody"
+  array<<"goody"
 
 end
 
 def add_element_to_start_of_array(array, element)
-["hi"]
-add_element_to_start_of_array.unshift ("will")
+array =["hi"]
+element = "will"
+array.unshift ("will")
 end
 
 def remove_element_from_end_of_array(array)
-["hello","good","well"]
-remove_element_from_end_of_array.pop
+array = ["hello","good","well"]
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-["hello","hi","good"]
-remove_element_from_start_of_array.shift
+array = ["hello","hi","good"]
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
   array =["hello","hi","how are you"]
-  retrieve_element_from_index[0]
+  index_number = 0
+  array[0]
 end
 
 def retrieve_first_element_from_array(array)
